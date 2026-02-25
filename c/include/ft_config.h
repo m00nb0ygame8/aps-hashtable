5ef3530fb060b9e50dd2b4a3a07158417b442c75
m00nb0ygame8 <52793898+m00nb0ygame8@users.noreply.github.com>
2026-02-25 02:52:23 -0500

    Update FT_LOAD_FACTOR from 0.85 to 0.8 to match new version

--- c/include/ft_config.h
+++ c/include/ft_config.h
@@ -1,10 +1,11 @@
 #ifndef FT_CONFIG_H
 #define FT_CONFIG_H
 
-#define FT_LOAD_FACTOR 0.85
+#define FT_LOAD_FACTOR 0.8
 
 #define FT_INIT_SIZE (1 << 13) //8192 buckets
 
 #define FT_GROWTH_FACTOR 4
+
 
 #endif
